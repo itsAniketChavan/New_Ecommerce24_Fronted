69d0f26d5021cadec5df4f9ee5ddc8869bbf4ece
AbhiGharpankar1722 <abhigharpankar1722@gmail.com>
2024-08-07 11:45:37 +0900

    some changes in footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const footer = () => {
   return (
     <div>
-      Footer
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptatem
+        amet, corporis provident velit eligendi fugit reiciendis autem
+        consequuntur animi reprehenderit voluptatibus blanditiis voluptatum.
+        Recusandae unde beatae libero facilis qui!
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default footer
+export default footer;
